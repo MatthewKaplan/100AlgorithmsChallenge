91f741672b8fc508b0dde577274949b7a188b10a
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-04-05 01:15:08 -0600

    Challenge 2 complete ... 98 more to go

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,24 @@
-function add(param1: number, param2: number): number {
+// 1. Write a function that returns the sum of two numbers.
 
+function add(param1: number, param2: number): number {
+    return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+// 2. Write a function that returns the sum of all numbers regardless of # of params.
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+function add2(...param1: number[]): number {
+    let total = 0;
+    param1.forEach( num => {    
+        return total += num;
+    })
+    return total;
+}
+
+
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
 
