d3200bd4d6cbefdd225a7776c5e8739f17d91fa5
Matthew Kaplan <kaplan.matthew.p@colorado.edu>
2019-04-08 21:18:31 -0600

    Completed challenge 5 ... 95 more to go

--- adjacentElementsProduct/adjacentElementsProduct.js
+++ adjacentElementsProduct/adjacentElementsProduct.js
@@ -1,5 +1,14 @@
 function adjacentElementsProduct(inputArray) {
 
+  let largestProduct = inputArray[0] * inputArray[1];
+
+  for(i=1; i < inputArray.length - 1; i++) {
+    let product = inputArray[i] * inputArray[i + 1];
+
+    largestProduct = largestProduct < product ? product : largestProduct;
+  }
+  return largestProduct;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, 100, 100, 7, 3]));
