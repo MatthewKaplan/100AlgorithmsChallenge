011740b16fe2bcfc2343b7157b259fae8dd23d4c
Matthew Kaplan <kaplan.matthew.p@colorado.edu>
2019-04-08 23:31:26 -0600

    challenge 8 complete ... 92 more to go

--- alphabeticShift/alphabeticShift.js
+++ alphabeticShift/alphabeticShift.js
@@ -1,5 +1,19 @@
 function alphabeticShift(inputString) {
 
+  let inputArray = inputString.split("");
+
+  console.log(inputArray);
+
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] == 'z') {
+      inputArray[i] = 'a';
+    } else {
+      inputArray[i] = String.fromCharCode(inputArray[i].charCodeAt(0) + 1);
+    }
+  }
+  return inputArray.join("")
 }
 
 console.log(alphabeticShift('crazy'));
+
+// Expected output: dsbaz
