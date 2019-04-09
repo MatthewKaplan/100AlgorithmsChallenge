fc008b583f260ffdc01d1c3497951f35b8076756
Matthew Kaplan <kaplan.matthew.p@colorado.edu>
2019-04-08 20:05:55 -0600

    challenge 3 complete...97 more togo

--- addBorder/addBorder.js
+++ addBorder/addBorder.js
@@ -1,5 +1,32 @@
 function addBorder(picture) {
 
+  // First were going to create a wall of asterisks(*) using the .repeat()
+  // method that is the length of the first index of the array (picture)
+  // and add 2 to that to get a wall that equals ('*****')
+
+  const wall = '*'.repeat(picture[0].length + 2)
+
+  // Now we need to use the 2 methods .push() and .unshift() to add the wall
+  // of asterisks(*) to the beginning and end of the array (picture). So that
+  // picture array looks like ['*****', 'abc', 'ded', '*****']
+
+  picture.push(wall)
+  picture.unshift(wall)
+
+  // Now we need to add an asterisks(*) to the beginning and end of the middle
+  // elements in the picture array. ('*abc*', '*ded*'). I will use a for loop
+  // and the concat method to accomplish this
+
+  for (let i=1; i < picture.length; i++){
+    picture[i] = '*'.concat(picture[i], '*');
+  }
+
+  // Return picture 
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["Matthew", "Kaplan"]));
+
+// Expected output: ['*****', '*abc*', '*ded*', '*****']
--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,27 @@
 function addBorder(picture: string[]): string[] {
 
+    // We first use .repeat() to create a wall of asterisks(*) that is the 
+    // length of the first index of the array ("abc") plus 2 so 
+    // wall = '*****'  
+
+    const wall = '*'.repeat(picture[0].length + 2);
+
+    // We then use unshift and push to add wall ('*****') to the beginning and
+    // end of the picture array. (['*****', 'abc', 'ded', '*****'])
+
+    picture.unshift(wall);
+    picture.push(wall);
+
+    // We then use a for loop to concat an asterisk on the beginning and end
+    // of the middle 2 elements in the array ('*abc*', '*ded*')
+
+    for(let i = 1; i < picture.length - 1; i++) {
+        picture[i] = '*'.concat(picture[i], '*');
+    }
+
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
+
+// Expected output: ['*****', '*abc*', '*ded*', '*****']
